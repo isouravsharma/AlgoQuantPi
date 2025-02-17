5ee2a8000cdb309d94d6a596adbaa1edf5c5a84a
isouravsharma <sourav.sharma@hitachivantara.com>
2025-02-17 00:20:02 -0600

    formula updated

--- Quant/.ipynb_checkpoints/Returns-checkpoint.py
+++ Quant/.ipynb_checkpoints/Returns-checkpoint.py
@@ -13,9 +13,10 @@ def cummulative_returns(data):
     data['d_rtn'] = data['Close'].pct_change()
     # data['w_rtn'] = data['Close'].resample('W').ffill().pct_change()
     # data['m_rtn'] = data['Close'].resample('M').ffill().pct_change()
-    data['cum_rtn'] = (data['d_rtn']+1).cumprod()
+    data['cum_rtn'] = ((1+ data['d_rtn']).cumprod() -1)*100
     return data
 
+# (1 + data['Daily Return']).cumprod() - 1
 
 # cummulative Return
 
