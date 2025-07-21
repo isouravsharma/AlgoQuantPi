9655c8699e0bc0076ea53a563e81b11dc365d6e2
isouravsharma <sourav.sharma@hitachivantara.com>
2025-07-20 22:00:48 -0600

    SQLite3 example tested
    Created SQLite example

--- Quant/signals.py
+++ Quant/signals.py
@@ -29,21 +29,24 @@ def stratgey2_momentum(row):
     else:
         return 0  # Hold
 
-def label_sd_categories(df, column='Distance_From_Mean'):
+
+
+
+def label_sd_with_signals(df, column='Distance_From_Mean'):
     mean = df[column].mean()
     std = df[column].std()
 
     def categorize(x):
         if x >= mean + 3 * std:
             return 'ABOVE 3 SD', 3
-        elif x >= mean + 2 * std:
-            return 'ABOVE 2 SD', 2
-        elif x >= mean + 1 * std:
-            return 'ABOVE 1 SD', 1
         elif x <= mean - 3 * std:
             return 'BELOW 3 SD', -3
+        elif x >= mean + 2 * std:
+            return 'ABOVE 2 SD', 2
         elif x <= mean - 2 * std:
             return 'BELOW 2 SD', -2
+        elif x >= mean + 1 * std:
+            return 'ABOVE 1 SD', 1
         elif x <= mean - 1 * std:
             return 'BELOW 1 SD', -1
         else:
@@ -51,7 +54,29 @@ def label_sd_categories(df, column='Distance_From_Mean'):
 
     labels_levels = df[column].apply(categorize)
     df[['Label', 'SD_Level']] = pd.DataFrame(labels_levels.tolist(), index=df.index)
+    return df
+
+
+def label_exit_signals(df):
+    df = df.copy()
+    df['Exit_Price'] = None
+    df['Exit_For_Entry'] = None
+
+    for i in range(len(df)):
+        entry_label = df.iloc[i]['Label']
+        entry_index = df.index[i]
+
+        if entry_label == 'BELOW 3 SD':
+            df.at[entry_index, 'Exit_Price'] = df.at[entry_index, 'UB']
+            df.at[entry_index, 'Exit_For_Entry'] = entry_index
+
+        elif entry_label == 'BELOW 2 SD':
+            df.at[entry_index, 'Exit_Price'] = df.at[entry_index, 'MB']
+            df.at[entry_index, 'Exit_For_Entry'] = entry_index
 
     return df
 
 
+
+
+        
